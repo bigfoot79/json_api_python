d28f25dfc38ab8e5abe9f242bc1c4344300c9bfb
neppar <neppar@gmail.com>
2018-08-22 13:57:50 +1000

    new module on branch

--- api_call.py
+++ api_call.py
@@ -5,7 +5,8 @@ from sys import argv
 script, end_point = argv
 
 def get_api(url):
-    response = requests.get("https://jsonplaceholder.typicode.com/todos")
+    response = requests.get(url)
     return json.loads(response.text)
 
 print(get_api(end_point))
+print(type(get_api(end_point)))
