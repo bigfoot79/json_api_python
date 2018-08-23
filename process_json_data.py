bd5a3801eb97ce8c48e674e03d14c357bd68185a
neppar <neppar@gmail.com>
2018-08-23 20:14:04 +1000

    module completed to calucation completion

--- process_json_data.py
+++ process_json_data.py
@@ -14,18 +14,25 @@ api_content = get_api(end_point)
 
 # counts the number of dictionaries included in the api_content list
 number_of_elements = len(api_content)
-
+# counter to iterate through the list of dictionaries
 i = 0
+
+# counter to count the number of False tasks
 not_completed = 0
 
+# for loop to iterate through the list of dictionaries and count the number
+# of dictionaries with key: 'completed' = False
 for element in api_content:
     dict_loop = api_content[i]
     if dict_loop['completed'] == False:
         not_completed += 1
     i += 1
 
+# calculates the number of completed tasks by subtracting the not_completed tasks
+# from the number_of_elements.  This provides the number of completed tasks
 completed_tasks = number_of_elements - not_completed
 
+# prints a variety of variables
 print("There are " + str(number_of_elements) + " todos.")
 print("There are " + str(not_completed) + " tasks which have not been completed")
 print("There are " + str(completed_tasks) + " which have been completed")
